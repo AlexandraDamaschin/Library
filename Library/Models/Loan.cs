b6854fb40a4377ca6517cb88f4cdca46ee591f9e
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2017-12-29 16:19:39 +0000

    Tables added

---
+++ Library/Models/Loan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public class Loan
+    {
+        //[ForeignKey("associatedCategory")]
+        //public int MemberId { get; set; }
+    }
+}
