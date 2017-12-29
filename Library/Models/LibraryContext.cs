78971cfe67fc50ea6063dbecd20fac21c00790b7
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2017-12-29 16:29:09 +0000

    Database created-code first

---
+++ Library/Models/LibraryContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext():base("DefaultConnection")
+        {
+         Configuration.LazyLoadingEnabled = false;
+        }
+
+    public DbSet<Member> Members { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Loan> Loans { get; set; }
+}
+}
--- Library/Models/Loan.cs
+++ Library/Models/Loan.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace Library.Models
 {
+    [Table("Loan")]
     public class Loan
     {
-        //[ForeignKey("associatedCategory")]
-        //public int MemberId { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Loan ID")]
+        public int LoanId { get; set; }
+
+        [ForeignKey("associatedCategory")]
+        public int MemberId { get; set; }
+
+        [ForeignKey("associatedBook")]
+        public int BookId { get; set; }
+
+        public virtual Member associatedMember { get; set; }
+
+        public virtual Book associatedBook { get; set; }
     }
 }
